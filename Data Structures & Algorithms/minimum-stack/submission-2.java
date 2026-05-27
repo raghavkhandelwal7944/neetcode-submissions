d046b4eca1bd3206b1b6e22496a971ef7217955e
raghavkhandelwal7944 <112472475+raghavkhandelwal7944@users.noreply.github.com>
2026-05-27 11:19:12 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,30 @@
+class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+    public MinStack() {
+        mainStack=new Stack<>();
+        minStack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        }else{
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
